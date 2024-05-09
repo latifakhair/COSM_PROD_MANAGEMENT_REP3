1f21d3e00aad018e0a3f323c7bada2e982c40299
utilisateur <latifakhair2@gmail.com>
2024-05-09 17:57:35 +0200

    Création de dépot+Login_interface

---
+++ src/cos_prods_management2/COS_PRODS_MANAGEMENT2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cos_prods_management2;
+
+/**
+ *
+ * @author utilisateur
+ */
+public class COS_PRODS_MANAGEMENT2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+    
+    
+}
